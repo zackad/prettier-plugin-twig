cab012ae25960f1d18b490a4a9f2d10a6d7997e6
Liam Rella <liam.rella@gmail.com>
2024-08-09 15:16:50 +0930

    replace failing tests

--- tests/Failing/jsfmt.spec.js
+++ tests/Failing/jsfmt.spec.js
@@ -1,5 +1,26 @@
-run_spec(__dirname, ["twig"], {
+import { run_spec } from "tests_config/run_spec";
+import { describe, expect, it } from "vitest";
+
+/** @type {import('tests_config/run_spec').PrettierOptions} */
+const formatOptions = {
     twigPrintWidth: 120,
     twigAlwaysBreakObjects: true,
     twigFollowOfficialCodingStandards: false
+};
+
+describe("Failing", () => {
+    it("should handle failing cases", async () => {
+        const { actual, snapshotFile } = await run_spec(import.meta.url, {
+            source: "failing.twig",
+            formatOptions
+        });
+        await expect(actual).toMatchFileSnapshot(snapshotFile);
+    });
+    it("should handle controversial cases", async () => {
+        const { actual, snapshotFile } = await run_spec(import.meta.url, {
+            source: "controversial.twig",
+            formatOptions
+        });
+        await expect(actual).toMatchFileSnapshot(snapshotFile);
+    });
 });
