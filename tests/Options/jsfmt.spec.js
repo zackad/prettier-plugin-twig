4e2f5cc3b3672d6e71941a3c66fd245c546fa817
Liam Rella <liam.rella@gmail.com>
2024-08-12 12:53:58 +0930

    replace options tests

--- tests/Options/jsfmt.spec.js
+++ tests/Options/jsfmt.spec.js
@@ -1,5 +1,32 @@
-run_spec(__dirname, ["twig"], {
-    twigAlwaysBreakObjects: false,
-    twigPrintWidth: 120,
-    twigOutputEndblockName: true
+import { run_spec } from "tests_config/run_spec";
+import { describe, expect, it } from "vitest";
+
+describe("Options", () => {
+    it("should not always break objects", async () => {
+        const { actual, snapshotFile } = await run_spec(import.meta.url, {
+            source: "alwaysBreakObjects.twig",
+            formatOptions: {
+                twigAlwaysBreakObjects: false
+            }
+        });
+        await expect(actual).toMatchFileSnapshot(snapshotFile);
+    });
+    it("should not output end block name", async () => {
+        const { actual, snapshotFile } = await run_spec(import.meta.url, {
+            source: "endblockName.twig",
+            formatOptions: {
+                twigOutputEndblockName: false
+            }
+        });
+        await expect(actual).toMatchFileSnapshot(snapshotFile);
+    });
+    it("should handle a non-standard print-width", async () => {
+        const { actual, snapshotFile } = await run_spec(import.meta.url, {
+            source: "printWidth.twig",
+            formatOptions: {
+                twigPrintWidth: 120
+            }
+        });
+        await expect(actual).toMatchFileSnapshot(snapshotFile);
+    });
 });
