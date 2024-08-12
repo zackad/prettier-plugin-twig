b28357b03c89dac21900e814d4576914b96970c7
Liam Rella <liam.rella@gmail.com>
2024-08-12 13:14:06 +0930

    replace twig coding standards tests

--- tests/TwigCodingStandards/jsfmt.spec.js
+++ tests/TwigCodingStandards/jsfmt.spec.js
@@ -1,1 +1,16 @@
-run_spec(__dirname, ["twig"], { twigAlwaysBreakObjects: false });
+import { run_spec } from "tests_config/run_spec";
+import { describe, expect, it } from "vitest";
+
+/** @type {import('tests_config/run_spec').PrettierOptions} */
+const formatOptions = {
+    twigAlwaysBreakObjects: false
+};
+
+describe("Twig coding standards", () => {
+    it("should follow twig coding standards", async () => {
+        const { actual, snapshotFile } = await run_spec(import.meta.url, {
+            source: "twigCodingStandards.twig"
+        });
+        await expect(actual).toMatchFileSnapshot(snapshotFile);
+    });
+});
