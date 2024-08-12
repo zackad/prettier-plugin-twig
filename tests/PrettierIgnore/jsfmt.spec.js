27beb238b340bcdaa9e6d27335c2c320f5872588
Liam Rella <liam.rella@gmail.com>
2024-08-12 12:56:48 +0930

    replace prettier ignore tests

--- tests/PrettierIgnore/jsfmt.spec.js
+++ tests/PrettierIgnore/jsfmt.spec.js
@@ -1,1 +1,17 @@
-run_spec(__dirname, ["twig"]);
+import { run_spec } from "tests_config/run_spec";
+import { describe, expect, it } from "vitest";
+
+describe("Prettier ignore", () => {
+    it("should handle single line ignore", async () => {
+        const { actual, snapshotFile } = await run_spec(import.meta.url, {
+            source: "prettierIgnore.twig"
+        });
+        await expect(actual).toMatchFileSnapshot(snapshotFile);
+    });
+    it("should handle ignore blocks", async () => {
+        const { actual, snapshotFile } = await run_spec(import.meta.url, {
+            source: "prettierIgnoreStartEnd.twig"
+        });
+        await expect(actual).toMatchFileSnapshot(snapshotFile);
+    });
+});
