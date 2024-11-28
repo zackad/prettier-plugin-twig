51da2dcadf5f26f02552176336d5eabbd94051f3
zackad <zackad.dev@gmail.com>
2024-11-28 07:52:28 +0700

    fix: pass option to printer

--- src/print/MemberExpression.js
+++ src/print/MemberExpression.js
@@ -5,13 +5,19 @@ import {
     wrapExpressionIfNeeded
 } from "../util/index.js";
 
-const { group } = doc.builders;
+const { group, softline, indent } = doc.builders;
 
 const printMemberExpression = (node, path, print) => {
     node[EXPRESSION_NEEDED] = false;
     node[STRING_NEEDS_QUOTES] = true;
     const parts = [path.call(print, "object")];
-    parts.push(node.computed ? "[" : ".");
+    if (node.computed) {
+        parts.push("[");
+    } else if (options.experimentalMethodChainIndentation) {
+        parts.push(indent([softline, "."]));
+    } else {
+        parts.push(".");
+    }
     parts.push(path.call(print, "property"));
     if (node.computed) {
         parts.push("]");
