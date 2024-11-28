8eaf4a3cbcab8cc4ccf08f50f3984e7b95b1530c
zackad <zackad.dev@gmail.com>
2024-11-28 07:05:33 +0700

    feat: introduce experimental option 'experimentalMethodChainIndentation'

--- tests/Experimental/experimental.spec.js
+++ tests/Experimental/experimental.spec.js
@@ -4,7 +4,10 @@ import { describe, expect, it } from "vitest";
 describe("Experimental", () => {
     it("Properly indent chain method", async () => {
         const { actual, snapshotFile } = await run_spec(import.meta.url, {
-            source: "chain_indentation.twig"
+            source: "chain_indentation.twig",
+            formatOptions: {
+                experimentalMethodChainIndentation: true
+            }
         });
         await expect(actual).toMatchFileSnapshot(snapshotFile);
     });
